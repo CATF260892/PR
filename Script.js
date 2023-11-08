efca0125fab2bbf340698fee4912a52ef1c1af0a
CATF260892 <142687570+CATF260892@users.noreply.github.com>
2023-11-08 02:06:31 -1200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded",(event)=> { 
+  console.log("DOM entièrement chargé et analysé");
+  setTimeout(function () {
+    document.getElementById("loader").style.top ="-100vh";
+  }, 1000)})
+
+   
+  
+
+  
